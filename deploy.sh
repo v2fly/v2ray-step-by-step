8584be840b82b33c7521921d6d2f4b63758c32d8
V2FlyContrib <SharedAccount+V2FlyContrib@unial.org>
2019-07-05 13:01:07 +0800

    update shared login credential into deploy script

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,8 @@ vuepress build .
 cd .vuepress/dist
 echo "guide.v2fly.org" > CNAME
 git init
+git config user.name V2FlyContrib
+git config user.email SharedAccount+V2FlyContrib@unial.org
 git add -A
 git commit -m "built at $(LANG=en_US date)"
 git push -f -v --progress git@github.com:v2fly/v2ray-step-by-step.git master:gh-pages
