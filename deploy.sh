81694a1305fc444e054c702898d7d073079f23a9
V2FlyContrib <SharedAccount+V2FlyContrib@unial.org>
2019-07-06 09:24:17 +0800

    pevent default gpg sign when deploy

--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,7 @@ echo "guide.v2fly.org" > CNAME
 git init
 git config user.name V2FlyContrib
 git config user.email SharedAccount+V2FlyContrib@unial.org
+git config commit.gpgsign false
 git add -A
 git commit -m "built at $(LANG=en_US date)"
 git push -f -v --progress git@github.com:v2fly/v2ray-step-by-step.git master:gh-pages
