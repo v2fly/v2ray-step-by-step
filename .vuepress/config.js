a8b958ba1c81e9eca4d6a76569a3f6d8ca0cda65
Dct Mei <dctxmei@gmail.com>
2020-03-28 17:10:52 +0800

    Remove multilingual list #145

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -165,37 +165,12 @@ module.exports = {
     '/': {
       lang: 'zh-CN',
       title: '新 V2Ray 白话文指南',
-      description: 'v2fly社区维护的ToutyRater版新手向教程。'
+      description: 'V2Fly 社区维护的新手向教程。'
     },
     '/en_US/': {
       lang: 'en-US',
       title: 'V2Ray Beginner\'s Guide',
       description: 'Step-by-step guide for first-timers\' using V2Ray.'
-    },
-    '/fa/': {
-      lang: 'fa-IR',
-      title: 'راهنمای مبتدی V2Ray',
-      description: 'راهنمای گام به گام برای اولین بار با استفاده از V2Ray.'
-    },
-    '/ko_KR/': {
-      lang: 'ko-KR',
-      title: 'V2Ray 초급 가이드',
-      description: '초보자를위한 V2Ray 사용을위한 단계별 안내서입니다.'
-    },
-    '/ru_RU/': {
-      lang: 'ru-RU',
-      title: 'V2Ray Руководство для начинающих',
-      description: 'Пошаговое руководство для новичков, использующих V2Ray.'
-    },
-    '/vi_VN/': {
-      lang: 'vi-VN',
-      title: 'Hướng dẫn cho người mới bắt đầu V2Ray',
-      description: 'Hướng dẫn từng bước cho những người lần đầu tiên sử dụng V2Ray.'
-    },
-    '/fr_FR/': {
-      lang: 'fr-FR',
-      title: 'Guide du débutant V2Ray',
-      description: 'Guide pas à pas pour les débutants utilisant V2Ray.'
     }
   },
   themeConfig: {
