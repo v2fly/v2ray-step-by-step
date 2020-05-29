c7e02729ea1c05ecb752d6594c941937b4642d6d
V2FlyContrib <SharedAccount+V2FlyContrib@unial.org>
2020-05-29 17:34:51 -0500

    replace build time with dev commit hash

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ vuepress build .
 
 echo -e "\n\nCheck if build prompted any errors.\nPress any key to countinue..."
 read
-
+DEPLOY_VER=$(git rev-parse --short HEAD)
 cd .vuepress/dist
 echo "guide.v2fly.org" > CNAME
 git init
@@ -13,7 +13,7 @@ git config user.name V2FlyContrib
 git config user.email SharedAccount+V2FlyContrib@unial.org
 git config commit.gpgsign false
 git add -A
-git commit -m "build $(date -u '+#%U%g%w-%N')"
+git commit -m "build dev@$DEPLOY_VER"
 git push -f -v --progress git@github.com:v2fly/v2ray-step-by-step.git master:gh-pages
 cd ../
 rm -rfv dist/
