47395b895848c471cd10fb8697aa370f001102ca
V2FlyContrib <SharedAccount+V2FlyContrib@unial.org>
2020-06-23 07:39:34 -0500

    new deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,21 +1,27 @@
 #!/bin/bash
-
-echo -e "Start building..."
+rm -rf .vuepress/dist
+echo -e "Cloning gh-pages...\n"
+git clone -v --branch gh-pages git@github.com:v2fly/v2ray-step-by-step.git ./.vuepress/old_dist
+rm -rv .vuepress/old_dist/*
+echo -e "Start building...\n"
 vuepress build .
-
-echo -e "\n\nCheck if build prompted any errors.\nPress any key to countinue..."
+DEV_VER=$(git rev-parse --short dev)
+TRANSIFEX_VER=$(git rev-parse --short transifex)
+COMMIT_MSG="dev@$DEV_VER / transifex@$TRANSIFEX_VER"
+echo "\nNext deployment will be noted as \"$COMMIT_MSG\""
+echo -e "\nCheck if build prompted any errors.\nPress any key to countinue..."
 read
-DEPLOY_VER=$(git rev-parse --short HEAD)
-cd .vuepress/dist
+cp -rpf .vuepress/dist/* .vuepress/old_dist/
+cd .vuepress/old_dist/
 echo "guide.v2fly.org" > CNAME
-git init
 git config user.name V2FlyContrib
 git config user.email SharedAccount+V2FlyContrib@unial.org
 git config commit.gpgsign false
 git add -A
-git commit -m "build dev@$DEPLOY_VER"
-git push -f -v --progress git@github.com:v2fly/v2ray-step-by-step.git master:gh-pages
+git commit -m "$COMMIT_MSG"
+read
+git push -f -v --progress git@github.com:v2fly/v2ray-step-by-step.git gh-pages:gh-pages
 cd ../
 rm -rfv dist/
+rm -rfv old_dist/
 echo "Done."
-
